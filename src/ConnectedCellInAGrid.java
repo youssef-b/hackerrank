05108e882790d7823a573698ab8137e1160671b5
youssef-b <youssef-b@users.noreply.github.com>
2017-05-07 20:49:06 +0200

    more solutions

---
+++ src/ConnectedCellInAGrid.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * https://www.hackerrank.com/challenges/connected-cell-in-a-grid
+ */
+public class ConnectedCellInAGrid {
+
+    public static class Cell {
+        int x, y;
+
+        public Cell(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] matrix = new int[n][m];
+        int[][] visited = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = in.nextInt();
+            }
+
+        }
+        int numberCellsLargestRegion = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int numberCells = numberCellsRegion(matrix, visited, i, j);
+                numberCellsLargestRegion = Math.max(numberCellsLargestRegion, numberCells);
+
+            }
+        }
+        System.out.println(numberCellsLargestRegion);
+    }
+
+    private static int numberCellsRegion(int[][] matrix, int[][] visited, int i, int j) {
+        int numberCells = 0;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        if (visited[i][j] == 1 || matrix[i][j] == 0) {
+            visited[i][j] = 1;
+            return 0;
+        }
+        Stack<Cell> stack = new Stack<>();
+        stack.push(new Cell(i, j));
+        while (!stack.isEmpty()) {
+            Cell currentCell = stack.pop();
+            int x = currentCell.x;
+            int y = currentCell.y;
+            if (visited[x][y] != 1) {
+                visited[x][y] = 1;
+
+                if (matrix[x][y] == 1) {
+                    numberCells += 1;
+
+                    if (x - 1 >= 0 && y - 1 >= 0) {
+                        stack.push(new Cell(x - 1, y - 1));
+                    }
+                    if (x - 1 >= 0) {
+                        stack.push(new Cell(x - 1, y));
+                    }
+                    if (x - 1 >= 0 && y + 1 < m) {
+                        stack.push(new Cell(x - 1, y + 1));
+                    }
+                    if (y - 1 >= 0) {
+                        stack.push(new Cell(x, y - 1));
+                    }
+                    if (y + 1 < m) {
+                        stack.push(new Cell(x, y + 1));
+                    }
+                    if (x + 1 < n && y - 1 >= 0) {
+                        stack.push(new Cell(x + 1, y - 1));
+                    }
+                    if (x + 1 < n) {
+                        stack.push(new Cell(x + 1, y));
+                    }
+                    if (x + 1 < n && y + 1 < m) {
+                        stack.push(new Cell(x + 1, y + 1));
+                    }
+                }
+            }
+        }
+        return numberCells;
+    }
+
+}
---
+++ src/MatrixRotationAlgo.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/matrix-rotation-algo
+ */
+public class MatrixRotationAlgo {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        int n = s.nextInt();
+        int r = s.nextInt();
+        int a[][] = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = s.nextInt();
+            }
+        }
+
+        List<Integer> layer = new ArrayList<>();
+        int iStart = 0;
+        int iEnd = m - 1;
+        int jStart = 0;
+        int jEnd = n - 1;
+        while (iEnd - iStart + 1 > 0 && jEnd - jStart + 1 > 0) {
+            extractMatrixLayer(a, layer, iStart, iEnd, jStart, jEnd);
+            Collections.rotate(layer, r % layer.size());
+            updateMatrixLayer(a, layer, iStart, iEnd, jStart, jEnd);
+            layer.clear();
+            iStart++;
+            iEnd--;
+            jStart++;
+            jEnd--;
+        }
+        for (int[] row : a) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void updateMatrixLayer(int[][] a, List<Integer> layer, int iStart, int iEnd, int jStart, int jEnd) {
+        int i= iStart;
+        int j= jStart;
+        int k = 0;
+        for (; i < iEnd; i++)
+            a[i][j] = layer.get(k++);
+        for (; j < jEnd; j++)
+            a[i][j] = layer.get(k++);
+        for (; i > iStart; i--)
+            a[i][j] = layer.get(k++);
+        for (; j > jStart; j--)
+            a[i][j] = layer.get(k++);
+    }
+
+    private static void extractMatrixLayer(int[][] a, List<Integer> layer, int iStart, int iEnd, int jStart, int jEnd) {
+        int i = iStart;
+        int j = jStart;
+        for (; i < iEnd; i++)
+            layer.add(a[i][j]);
+        for (; j < jEnd; j++)
+            layer.add(a[i][j]);
+        for (; i > iStart; i--)
+            layer.add(a[i][j]);
+        for (; j > jStart; j--)
+            layer.add(a[i][j]);
+    }
+}
